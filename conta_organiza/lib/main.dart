c5d789c499e34b61e0140e007e860517b6ec0dc1
Ramias <ramiaslopes@gmail.com>
2024-04-07 20:42:26 -0300

    assets adionado

--- conta_organiza/lib/main.dart
+++ conta_organiza/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:conta_organiza/tela_inicial.dart'; // Importe o arquivo da Tela Inicial
+import 'package:conta_organiza/Tela_inicial.dart'; // Importe o arquivo da Tela Inicial
 
 void main() {
   runApp(MyApp());
@@ -12,9 +12,12 @@ class MyApp extends StatelessWidget {
       title: 'Conta Organiza',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        //accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
       ),
       home:
-          TelaInicial(), // Aqui você usa a TelaInicial como a tela inicial do app
+          Tela_inicial(), // Aqui você usa a Tela_inicial como a tela inicial do app
     );
   }
 }
