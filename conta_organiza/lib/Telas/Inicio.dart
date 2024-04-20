6668925da4096ed746df2d7a036ca4903186055e
Ramias <ramiaslopes@gmail.com>
2024-04-19 23:31:18 -0300

    TelaInicio
    Alinhando texto mais logo

--- conta_organiza/lib/Telas/Inicio.dart
+++ conta_organiza/lib/Telas/Inicio.dart
@@ -1,34 +1,45 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class TelaInicial extends StatelessWidget {
+  bool queroEntar = true;
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double imageHeight =
-        screenHeight * 0.7; // Ajuste esta porcentagem conforme necessário
+        screenHeight * 0.2; // Ajuste esta porcentagem conforme necessário
 
     return MaterialApp(
       title: 'Imagem na Build',
       home: Scaffold(
-        body: Container(
-          alignment: Alignment
-              .topCenter, // Alinha a imagem no topo centralizado horizontalmente
-
-          child: Center(
-            child: Column(
-              children: [
-                Image.asset(
-                  alignment: Alignment.topCenter,
-                  'assets/images/logoCO.jpg',
-                  height: imageHeight,
-                  //height: 150, // Altura da imagem (ajuste conforme necessário)
-                  width: 150, // Largura da imagem (ajuste conforme necessário)
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 60),
+                    Image.asset("assets/images/logoCO.jpg",
+                        height: imageHeight),
+                    SizedBox(height: 20),
+                    const Text(
+                      "CONTA ORGANIZA",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "inter",
+                        fontSize: 32,
+                        color: Color(0xff838DFF),
+                      ),
+                    )
+                  ],
                 ),
-                //SizedBox(height: 20),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
