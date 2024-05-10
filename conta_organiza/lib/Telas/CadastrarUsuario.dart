d96a238a89e9a420e95261ea04ae1d1c3cb6a7ee
Ramias <ramiaslopes@gmail.com>
2024-05-10 20:43:38 -0300

    Cadastrar Usuário
    Visualmente pronto

--- conta_organiza/lib/Telas/CadastrarUsuario.dart
+++ conta_organiza/lib/Telas/CadastrarUsuario.dart
@@ -5,9 +5,9 @@ class CadastrarUsuario extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xff838DFF),
+        backgroundColor: const Color(0xff838DFF),
         bottom: PreferredSize(
-          preferredSize: Size.fromHeight(15),
+          preferredSize: const Size.fromHeight(15),
           child: Column(
             children: [
               Container(
@@ -31,7 +31,160 @@ class CadastrarUsuario extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(), // Aqui você pode adicionar os outros widgets da tela
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Nome completo',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black, // Mudança de cor
+                fontFamily: 'Inter', // Mudança de fonte
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xff838DFF), width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 14),
+            const Text(
+              'E-mail',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black, // Mudança de cor
+                fontFamily: 'Inter', // Mudança de fonte
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xff838DFF), width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Nome usuário',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontFamily: 'Inter',
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xff838DFF), width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Senha',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontFamily: 'Inter',
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xff838DFF), width: 2),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  side: const BorderSide(
+                    width: 4.0,
+                    color: Color(0xff000D63),
+                  ),
+                  backgroundColor: const Color(0xff5E6DDB),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20), // Ajusta o padding horizontal
+                  minimumSize: const Size(240, 55), // Largura e altura mínimas
+                ),
+                onPressed: () {
+                  // Função a ser executada quando o botão for pressionado
+                },
+                child: const Text(
+                  "Cadastrar",
+                  style: TextStyle(
+                    color: Color(0xffffffff),
+                    fontSize: 20,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  side: const BorderSide(
+                    width: 4.0,
+                    color: Color(0xff000D63),
+                  ),
+                  backgroundColor: const Color(0xff5E6DDB),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 8), // Ajuste o padding horizontal e vertical
+                  minimumSize: const Size(140, 40), // Largura e altura mínimas
+                ),
+                onPressed: () {
+                  // Função a ser executada quando o botão for pressionado
+                },
+                child: const Text(
+                  "Cancelar",
+                  style: TextStyle(
+                    color: Color(0xffffffff),
+                    fontSize: 14,
+                    fontFamily: 'Inter',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
