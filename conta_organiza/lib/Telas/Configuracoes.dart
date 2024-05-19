a4a69acbc433ad6bf465b9e81addc61602100681
Ramias <ramiaslopes@gmail.com>
2024-05-19 19:44:14 -0300

    Tela Configurações
    Pronta só falta adicionar função para para os botões

--- conta_organiza/lib/Telas/Configuracoes.dart
+++ conta_organiza/lib/Telas/Configuracoes.dart
@@ -9,76 +9,91 @@ class _ConfiguracoesState extends State<Configuracoes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        ListTile(
-          leading: Container(
-            margin: EdgeInsets.symmetric(horizontal: 6.0),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                width: 1.0,
-                color: Colors.black,
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Container(
+              margin: EdgeInsets.symmetric(horizontal: 0.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.black,
+                ),
+              ),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/Foto do perfil.png'),
               ),
             ),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/Foto do perfil.png'),
+            title: Text('Perfil'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              // Navegue para a tela de configurações de perfil
+            },
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.black)),
             ),
           ),
-          title: Text('Perfil'),
-          trailing: Icon(Icons.chevron_right),
-          onTap: () {
-            // Navegue para a tela de configurações de perfil
-          },
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 6.0),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(
-              width: 1.0,
-              color: Colors.black,
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/Icone Conta.png'),
+            ),
+            title: Text('Conta'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              // Navegue para a tela de configurações de conta
+            },
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.black)),
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 6.0),
-          decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(color: Colors.black)),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage:
+                  AssetImage('assets/images/Icone Notificacao.png'),
+            ),
+            title: Text('Notificações'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              // Navegue para a tela de configurações de notificações
+            },
           ),
-        ),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage('assets/images/Icone Conta.png'),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.black)),
+            ),
           ),
-          title: Text('Conta'),
-          trailing: Icon(Icons.chevron_right),
-          onTap: () {
-            // Navegue para a tela de configurações de perfil
-          },
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 6.0),
-          decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(color: Colors.black)),
+          const SizedBox(height: 20),
+          Center(
+            child: ElevatedButton.icon(
+              onPressed: () {
+                // Ação para deslogar da conta
+              },
+              icon: Icon(
+                Icons.power_settings_new,
+                color: Colors.black, // Cor do ícone preto
+              ),
+              label: Text(
+                'Deslogar',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  color: Colors.black,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff838dff), // Cor de fundo roxa
+              ),
+            ),
           ),
-        ),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundImage: AssetImage('assets/images/Icone Notificacao.png'),
-          ),
-          title: Text('Notificações'),
-          trailing: Icon(Icons.chevron_right),
-          onTap: () {
-            // Navegue para a tela de configurações de perfil
-          },
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 6.0),
-          decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(color: Colors.black)),
-          ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
