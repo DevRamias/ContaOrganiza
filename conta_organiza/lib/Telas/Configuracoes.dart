0b2b07e8a5d9d79c233814c9717c61c92d476700
Ramias <ramiaslopes@gmail.com>
2024-05-19 18:23:50 -0300

    Botões ajustado
    Parte visual dos botões pronto

--- conta_organiza/lib/Telas/Configuracoes.dart
+++ conta_organiza/lib/Telas/Configuracoes.dart
@@ -9,33 +9,76 @@ class _ConfiguracoesState extends State<Configuracoes> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(
-                Icons.notifications), // Adiciona o ícone na frente do texto
-            title: Text('Notificações'),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () {
-              // Navegue para a tela de configurações de notificações
-            },
+      body: ListView(children: [
+        ListTile(
+          leading: Container(
+            margin: EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 1.0,
+                color: Colors.black,
+              ),
+            ),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/Foto do perfil.png'),
+            ),
           ),
-          ListTile(
-            title: Text('Privacidade'),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () {
-              // Navegue para a tela de configurações de privacidade
-            },
+          title: Text('Perfil'),
+          trailing: Icon(Icons.chevron_right),
+          onTap: () {
+            // Navegue para a tela de configurações de perfil
+          },
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              width: 1.0,
+              color: Colors.black,
+            ),
           ),
-          ListTile(
-            title: Text('Sobre'),
-            trailing: Icon(Icons.chevron_right),
-            onTap: () {
-              // Navegue para a tela de informações sobre o aplicativo
-            },
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black)),
           ),
-        ],
-      ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/Icone Conta.png'),
+          ),
+          title: Text('Conta'),
+          trailing: Icon(Icons.chevron_right),
+          onTap: () {
+            // Navegue para a tela de configurações de perfil
+          },
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black)),
+          ),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/Icone Notificacao.png'),
+          ),
+          title: Text('Notificações'),
+          trailing: Icon(Icons.chevron_right),
+          onTap: () {
+            // Navegue para a tela de configurações de perfil
+          },
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 6.0),
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.black)),
+          ),
+        ),
+      ]),
     );
   }
 }
